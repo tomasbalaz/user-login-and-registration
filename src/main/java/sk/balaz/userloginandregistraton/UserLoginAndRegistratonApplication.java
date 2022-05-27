a6abd44116e69656ea74cdd30fd301eb50bafc3f
Tomas Balaz <tomasba@amdocs.com>
2022-05-27 12:12:00 +0200

    Created user-login-and-registraton application

---
+++ src/main/java/sk/balaz/userloginandregistraton/UserLoginAndRegistratonApplication.java
@@ -0,0 +1,13 @@
+package sk.balaz.userloginandregistraton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginAndRegistratonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginAndRegistratonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/balaz/userloginandregistraton/UserLoginAndRegistratonApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.userloginandregistraton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginAndRegistratonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
