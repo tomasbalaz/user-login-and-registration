1546381a8ea2375891c70a496ebf1de437cfd739
Tomas Balaz <tomasba@amdocs.com>
2022-05-30 09:33:28 +0200

    Created RegistrationController with register method

---
+++ src/main/java/sk/balaz/userloginandregistraton/registration/RegistrationController.java
@@ -0,0 +1,18 @@
+package sk.balaz.userloginandregistraton.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest registrationRequest) {
+        return  registrationService.register(registrationRequest);
+    }
+}
